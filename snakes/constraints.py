debf0bc3ce11c78a442eeeea0c607ffc51d3e860
jang <jang@ioctl.org>
2021-02-14 15:44:40 +0000

    Constrain the head and tail to not be adjacent

--- snakes/constraints.py
+++ snakes/constraints.py
@@ -1,9 +1,18 @@
-from z3 import Not, And, Or
+from z3 import Not, And, Or, If
 
 
 def constrain(g):
     place_head(g)
     place_tail(g)
+    head_and_tail_distinct(g)
+
+
+def Abs(x):
+    return If(x < 0, -x, x)
+
+
+def Manh(x1, y1, x2, y2):
+    return Abs(x1 - x2) + Abs(y1 - y2)
 
 
 def place_endpoint(g, ex, ey):
@@ -30,3 +39,7 @@ def place_tail(g):
     place_endpoint(g, *g.tail())
 
 
+def head_and_tail_distinct(g):
+    hx, hy = g.head()
+    tx, ty = g.tail()
+    g.add(Manh(hx, hy, tx, ty) > 1)
