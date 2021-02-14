032238965ef022471b271bd81cea23bfecb2e36c
jang <jang@ioctl.org>
2021-02-14 15:18:03 +0000

    Place the tail

--- snakes/constraints.py
+++ snakes/constraints.py
@@ -3,20 +3,30 @@ from z3 import Not, And, Or
 
 def constrain(g):
     place_head(g)
+    place_tail(g)
 
 
-def place_head(g):
-    # The head must be on the grid
-    hx, hy = g.head()
-    g.add(0 <= hx)
-    g.add(hx < g.width)
-    g.add(0 <= hy)
-    g.add(hy < g.height)
+def place_endpoint(g, ex, ey):
+    # The endpoint must be on the grid
+    g.add(0 <= ex)
+    g.add(ex < g.width)
+    g.add(0 <= ey)
+    g.add(ey < g.height)
 
-    # The head must not be on a marked cell
+    # The endpoint must not be on a marked cell
     for y in range(g.height):
         for x in range(g.width):
             if g.snake_possible(x, y):
                 continue
-            # The head can't be here.
-            g.add(Or(hx != x, hy != y))
+            # The endpoint can't be here.
+            g.add(Or(ex != x, ey != y))
+
+
+def place_head(g):
+    place_endpoint(g, *g.head())
+
+
+def place_tail(g):
+    place_endpoint(g, *g.tail())
+
+
